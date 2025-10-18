d5d4cc56097acaff6c0f78b698f983101afa3683
geomasilva <geovanasilva_maria@outlook.com>
2025-10-17 21:28:17 -0300

    Aula 17.10.25

---
+++ Aula - 17.10.25/js/meuscriot.js
@@ -0,0 +1,11 @@
+function exibir(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = "Exibido Nome!!!!"
+}
+
+function ocultar(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = ""
+}
